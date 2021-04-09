8a69092faddc65db12ced25d42512448c527f132
mauriciodittrichineves <mauraumdev@gmail.com>
2021-04-09 18:01:08 -0300

    commit inicial

---
+++ homestock/src/test/java/com/homestock/homestock/HomestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homestock.homestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
