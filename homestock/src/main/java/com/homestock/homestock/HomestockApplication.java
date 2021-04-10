f27d4695524c9ab412604835a4fad4dadcea31e5
mauriciodittrichineves <mauraumdev@gmail.com>
2021-04-10 16:18:32 -0300

    Validacao do cpf não testada

--- homestock/src/main/java/com/homestock/homestock/HomestockApplication.java
+++ homestock/src/main/java/com/homestock/homestock/HomestockApplication.java
@@ -1,5 +1,6 @@
 package com.homestock.homestock;
 
+import com.homestock.homestock.entitites.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- homestock/src/main/java/com/homestock/homestock/entitites/User.java
+++ homestock/src/main/java/com/homestock/homestock/entitites/User.java
@@ -59,4 +59,45 @@ public class User {
     public int hashCode() {
         return Objects.hash(name, email, cpf, password);
     }
+
+    public boolean cpfValidated(String cpf){
+
+
+        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") || cpf.equals("33333333333") || cpf.equals("44444444444") ||
+                cpf.equals("55555555555") || cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") || cpf.equals("99999999999") ||
+                cpf.length()!=11) return false;
+
+        //Separamos os digitos verificadores
+        char dig10, dig11;
+        dig10 = cpf.charAt(9);
+        dig11 = cpf.charAt(10);
+        boolean verificado1;
+        boolean verificado2;
+        // Calculando o primeiro digito verificador
+        verificado1 = calculadoraDeVerificador(10,dig10,cpf);
+        verificado2 = calculadoraDeVerificador(11,dig11,cpf);
+
+        if(verificado1 == true && verificado2 ==true){
+            return true;
+        }else return false;
+
+
+    }
+
+    public boolean calculadoraDeVerificador(int numeroDigitos, int digitoVerificador, String cpf){
+        int resultadoMultiplicacao;
+        int contadorCaractere = 0;
+        double digitoCalculado = 0;
+        while (numeroDigitos >= 2){
+            resultadoMultiplicacao = numeroDigitos * cpf.charAt(contadorCaractere);
+            numeroDigitos--;
+            contadorCaractere++;
+            digitoCalculado = digitoCalculado + resultadoMultiplicacao;
+        }
+        if (digitoCalculado == 10){digitoCalculado = 0;};
+        if (digitoCalculado == digitoVerificador){
+            return true;
+        };
+        return false;
+    }
 }
